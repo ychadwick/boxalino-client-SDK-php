ccbb888dc1c8a354a9970f3f3c26268a2530f3c6
Yorkie Chadwick <ychadwick@gmail.com>
2016-02-25 14:45:24 +0100

    correct lib path

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
-$libPath = './lib'; //path to the lib folder with the Boxalino Client SDK and PHP Thrift Client files
+$libPath = 'lib'; //path to the lib folder with the Boxalino Client SDK and PHP Thrift Client files
 require_once("lib/BxClient.php");
 use com\boxalino\bxclient\v1\BxClient;
 BxClient::LOAD_CLASSES($libPath);
